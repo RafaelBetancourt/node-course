ada47a1523c07949f33399469733de9f12e56b2b
RafaelBetancourt <rafael18282@gmail.com>
2023-01-28 22:06:48 -0500

    curso node 2da clase

--- monohilo.js
+++ monohilo.js
@@ -3,3 +3,11 @@ console.log("Hola node");
 setInterval(() => {
     console.log("Sigo ejjecutando");
 }, 1000);
+
+setInterval(() => {
+    console.log("Sigo ejjecutando");
+}, 1000);
+
+setInterval(() => {
+    console.log("Sigo ejjecutando");
+}, 1000);
