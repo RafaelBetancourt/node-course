b0ef6efba9b39aa322673d061eb148bbe56eb18a
RafaelBetancourt <rafael18282@gmail.com>
2023-01-30 15:24:23 -0500

    curso node callbacks

---
+++ async/callback.js
@@ -0,0 +1,23 @@
+function hola(nombre, miCallback) {
+  setTimeout(function () {
+    console.log("Hola, " + nombre);
+    miCallback();
+  }, 2000);
+}
+
+function adios(nombre, otroCallback) {
+  setTimeout(function () {
+    console.log("Adios, " + nombre);
+    otroCallback();
+  }, 1000);
+}
+
+console.log("Iniciando proceso...");
+hola("Rafa", function () {
+  adios("Maria", function () {
+    console.log("terminando proceso...");
+  });
+});
+
+// hola('Jose', function(){});
+// adios('Juana', function(){});
